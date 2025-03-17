44d9bba7e2bbb6263f702a06e9d538144230cd97
kuskyst <126965999+kuskyst@users.noreply.github.com>
2025-03-17 22:49:43 +0900

    fffff

--- app/src/main/java/jp/kuskyst/chat_sample_android/activity/MainActivity.kt
+++ app/src/main/java/jp/kuskyst/chat_sample_android/activity/MainActivity.kt
@@ -3,15 +3,20 @@ package jp.kuskyst.chat_sample_android.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import jp.kuskyst.chat_sample_android.ui.ChatScreen
+import jp.kuskyst.chat_sample_android.viewmodel.ChatViewModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: ChatViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ChatScreen()
+            ChatScreen(viewModel)
         }
     }
 }
--- app/src/main/java/jp/kuskyst/chat_sample_android/ui/ChatScreen.kt
+++ app/src/main/java/jp/kuskyst/chat_sample_android/ui/ChatScreen.kt
@@ -2,18 +2,17 @@ package jp.kuskyst.chat_sample_android.ui
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import jp.kuskyst.chat_sample_android.viewmodel.ChatViewModel
 
 @Composable
-fun ChatScreen(viewModel: ChatViewModel = viewModel()) {
+fun ChatScreen(viewModel: ChatViewModel) {
     val messages by viewModel.messages.collectAsState()
 
     var input by remember { mutableStateOf(TextFieldValue("")) }
